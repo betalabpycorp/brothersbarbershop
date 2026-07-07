9b2da65fed0e5058cfbb84829dc8d6f0e70e7024
betalabpycorp <betalab.py@gmail.com>
2026-07-07 15:32:28 -0300

    Add files via upload

---
+++ js/direcciones.js
@@ -0,0 +1,23 @@
+function irServicios() {
+    window.location.href = 'servicios.html';
+}
+
+function irNosotros() {
+    window.location.href = 'nosotros.html';
+}
+
+function irNovedades() {
+    window.location.href = 'noticias.html';
+}
+
+function irContacto() {
+    window.location.href = 'contactos.html';
+}
+
+function irGaleria() {
+    window.location.href = 'galeria.html';
+}
+
+function irInicio() {
+    window.location.href = 'index.html';
+}
